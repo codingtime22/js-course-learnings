b7f69dd2510f819ae78c1387dab530308a0a9d2a
SSP <silvan.sprenger@avm.swiss>
2025-09-04 15:13:35 +0200

    Save cart in LocalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+	cart = [{
 	productsId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
 	quantity: 2,
 }, {
 	productsId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
 	quantity: 1
 }];
+}
+
+function safeToLocalStorage() {
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productsId) {
 	let matchingItem;
@@ -23,6 +32,8 @@ export function addToCart(productsId) {
 			quantity: 1
 		});
 	}
+
+	safeToLocalStorage();
 }
 
 export function removeFromCart(productId) {
@@ -35,4 +46,6 @@ export function removeFromCart(productId) {
 	});
 
 	cart = newCart;
+
+	safeToLocalStorage();
 }
