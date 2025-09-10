139d9a0519068b085f32a2b0e9f97fa40a1d7560
SSP <silvan.sprenger@avm.swiss>
2025-09-10 10:54:26 +0200

    Created a feature to update the ammount of one product.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,9 @@ function renderCheckout() {
             <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
             <div class="product-quantity">
               <span>Quantity: <span class="quantity-label">${cartItem.quantity}</span></span>
-              <span class="update-quantity-link link-primary">Update</span>
+              <span class="update-quantity-link link-primary js-update-link" data-product-id="${cartItem.productId}">Update</span>
+              <input class="quantity-input" data-product-id="${cartItem.productId}"></input>
+              <span class="save-quantity-link link-primary" data-product-id="${cartItem.productId}">Save</span>
               <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${cartItem.productId}">Delete</span>
             </div>
           </div>
@@ -43,6 +45,36 @@ function renderCheckout() {
       removeFromCart(productId);
       document.querySelector(`.js-cart-item-container-${productId}`)?.remove();
       updateCartQuantity();
+    });
+  });
+
+  document.querySelectorAll('.js-update-link').forEach((link2) => {
+    link2.addEventListener('click', () => {
+      const productId2 = link2.dataset.productId;
+      const individualContainer = document.querySelector(`.js-cart-item-container-${productId2}`);
+      individualContainer.classList.add('is-editing-quantity');
+    });
+  });
+
+  document.querySelectorAll('.save-quantity-link').forEach((link3) => {
+    link3.addEventListener('click', () => {
+      const productId2 = link3.dataset.productId;
+      
+      const inputFieldParent = document.querySelector(`.js-cart-item-container-${productId2}`);
+      const inputFieldChild = inputFieldParent.querySelector('.quantity-input');
+      
+      const inputValueNumber = Number(inputFieldChild.value); 
+      
+      const cartItem = cart.find(item => item.productId === productId2)
+
+      if (cartItem) {
+        cartItem.quantity = inputValueNumber;
+        updateCartQuantity();
+
+        inputFieldParent.querySelector('.quantity-label').innerText = cartItem.quantity;
+      }
+
+      inputFieldParent.classList.remove('is-editing-quantity');
     });
   });
 }
